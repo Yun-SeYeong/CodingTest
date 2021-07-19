1ee7513d86dbe6e3f8e5d176f74e82da362457e0
Yun-SeYeong <dbstpdud09@naver.com>
2021-07-19 22:06:50 +0900

    update

--- session9/test6.py
+++ session9/test6.py
@@ -1,34 +1,36 @@
 def get_num(m, c):
-    print(c)
+    if len(c) <= 0:
+        return m
     result = []
 
     for c_pos in c:
-        m[c_pos[0]+1][c_pos[1]] = 2
-        if m[c_pos[0]+1][c_pos[1]] == 1:
+        if m[c_pos[0]][c_pos[1]] == 1:
+            m[c_pos[0]][c_pos[1]] = 2
+
+        if c_pos[0] < len(m)-1 and m[c_pos[0]+1][c_pos[1]] == 1:
             result.append([c_pos[0]+1, c_pos[1]])
 
-        if m[c_pos[0]-1][c_pos[1]] == 1:
+        if c_pos[0] > 0 and m[c_pos[0]-1][c_pos[1]] == 1:
             result.append([c_pos[0]-1, c_pos[1]])
 
-        if m[c_pos[0]][c_pos[1]+1] == 1:
+        if c_pos[1] < len(m)-1 and m[c_pos[0]][c_pos[1]+1] == 1:
             result.append([c_pos[0], c_pos[1]+1])
 
-        if m[c_pos[0]][c_pos[1]-1] == 1:
+        if c_pos[1] > 0 and m[c_pos[0]][c_pos[1]-1] == 1:
             result.append([c_pos[0], c_pos[1]-1])
 
-        if m[c_pos[0]+1][c_pos[1]+1] == 1:
+        if c_pos[0] < len(m)-1 and c_pos[1] < len(m)-1 and m[c_pos[0]+1][c_pos[1]+1] == 1:
             result.append([c_pos[0]+1, c_pos[1]+1])
 
-        if m[c_pos[0]-1][c_pos[1]-1] == 1:
+        if c_pos[0] > 0 and c_pos[1] > 0 and m[c_pos[0]-1][c_pos[1]-1] == 1:
             result.append([c_pos[0]-1, c_pos[1]-1])
 
-        if m[c_pos[0]+1][c_pos[1]-1] == 1:
+        if c_pos[0] < len(m)-1 and c_pos[1] > 0 and m[c_pos[0]+1][c_pos[1]-1] == 1:
             result.append([c_pos[0]+1, c_pos[1]-1])
 
-        if m[c_pos[0]-1][c_pos[1]+1] == 1:
+        if c_pos[0] > 0 and c_pos[1] < len(m)-1 and m[c_pos[0]-1][c_pos[1]+1] == 1:
             result.append([c_pos[0]-1, c_pos[1]+1])
-
-    get_num(m, result)
+    m = get_num(m, result)
 
     return m
 
@@ -42,11 +44,12 @@ if __name__ == '__main__':
 
     print(m)
 
-    result = 2
+    result = 0
 
     for i in range(n):
         for j in range(n):
             if m[i][j] == 1:
                 m = get_num(m, [[i, j]])
+                result += 1
 
-    print(m)
+    print(result)
